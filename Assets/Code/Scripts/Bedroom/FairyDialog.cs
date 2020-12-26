ebcf49a77b05c4514bb70a0e59d8e42d0dad34c9
melanie <melanie@cooperbold.com>
2020-12-26 16:07:56 -0700

    Get Fairy dialog working :sparkles:

---
+++ Assets/Code/Scripts/Bedroom/FairyDialog.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FairyDialog : MonoBehaviour
+{
+    public GameObject dialogBox;
+    public string knotName;
+
+    void OnEnable()
+    {
+        Debug.Log("FairyDialog is enabled!");
+        if (!dialogBox.activeInHierarchy)
+        {
+            NPCDialogue npcDialogue = dialogBox.GetComponent<NPCDialogue>();
+
+            npcDialogue.ShowDialog(knotName);
+        }
+    }
+}
