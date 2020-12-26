f65d252a20e34ea849e0aec45f4cd1c85f7f87e2
melanie <melanie@cooperbold.com>
2020-12-26 16:50:20 -0700

    Scene transitions, item pickup on click

---
+++ Assets/Code/Scripts/Bedroom/TransitionBedroom.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionBedroom : MonoBehaviour
+{
+    private void OnDisable()
+    {
+        GameManager.Instance.LoadScene("Level/Scenes/Farm/Home/Bedroom", new Vector2(-0.68f, -0.36f), new Vector3(-1.65f, -2.72f, -10), new Vector2(0, -1));
+    }
+}
