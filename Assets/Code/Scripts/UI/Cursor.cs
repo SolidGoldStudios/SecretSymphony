4fec4ea1240d4e38e8a70bc85b17f199701b54b3
melanie <melanie@cooperbold.com>
2020-12-26 20:37:27 -0700

    Add a cursor, but not a very good one

---
+++ Assets/Code/Scripts/UI/Cursor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    void Start()
+    {
+        //Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = cursorPos;
+    }
+}
