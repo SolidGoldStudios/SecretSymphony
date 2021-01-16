709b4402ea7cfdab4a1195996dda8c1cfed8c95f
melanie <melanie@cooperbold.com>
2021-01-16 12:51:58 -0700

    Migrate MusicPlayer script

---
+++ Assets/Code/Scripts/MusicPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private string song = "";
+    private string OdeToJoy = "EEFGFEDDEFED";
+
+    public void PressKey(string key)
+    {
+        Debug.Log("Pressed the " + key + " key!");
+
+        song += key;
+
+        Debug.Log("Song so far: " + song);
+
+        if (song.Equals(OdeToJoy))
+        {
+            Debug.Log("Finished the song!");
+            song = "";
+            GameManager.Instance.inkStory.variablesState["has_played_piano"] = true;
+            GameManager.Instance.HideMusicPlayer();
+            GameManager.Instance.ShowTooltipWithTimeout("You played the song!");
+
+        }
+        else if (song.Equals(OdeToJoy.Substring(0, song.Length)))
+        {
+            Debug.Log("Playing it right!");
+        }
+        else
+        {
+            Debug.Log("You fucked up!");
+            song = "";
+        }
+
+    }
+}
