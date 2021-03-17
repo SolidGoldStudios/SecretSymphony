b39237baa3e64fbb6682baed38ac29205a768f7a
Andrew Snyder <snyder.andrew.it2@gmail.com>
2021-03-17 11:54:38 -0400

    Rudimentary Settings Menu with Volume Controller
    Created a simple settings menu and volume controller.  Needs design work (you can do a lot with the slider or create a custom one).  Volume control works and saves in PlayerPrefs which is good for overall game settings such as this.  Will carry from scene to scene and even on new loads.  Not sure how much we want a save/load in here, so will discuss that more in depth on Saturday.

---
+++ Assets/Code/Scripts/UI/SettingsMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+	public Slider volumeSlider;
+	
+    void Start()
+    {
+		volumeSlider.value = PlayerPrefs.GetFloat("gameVolume");
+    }
+
+    public void ChangeVolume()
+	{
+		PlayerPrefs.SetFloat("gameVolume", volumeSlider.value);
+		PlayerPrefs.Save();
+		AudioListener.volume = PlayerPrefs.GetFloat("gameVolume");
+	}
+}
