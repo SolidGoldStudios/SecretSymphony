337b27441ee6d69c091af3cc0837eebc6e79ce02
ploppowaffles <30304558+ploppowaffles@users.noreply.github.com>
2021-03-17 20:09:31 -0700

    work on fairy following melody

---
+++ Assets/Code/Scripts/FairyFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FairyFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothTime = 0.3f;
+    private Vector3 velocity = Vector3.zero;
+
+    void Start()
+    {
+        transform.position = target.TransformPoint(new Vector3(-1f, 3f, 0f));
+    }
+
+    void Update()
+    {
+        Vector3 targetPosition = target.TransformPoint(new Vector3(-1f, 3f, 0f));
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
