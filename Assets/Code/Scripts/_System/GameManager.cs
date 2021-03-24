c323d45abed593cd75ca66ea168f515a4bf0cbc0
Andrew Snyder <snyder.andrew.it2@gmail.com>
2021-03-24 11:41:12 -0400

    General Bugfixing and Organizing - More in Dsecription.
    Lot of 'changes' but most of them are just reorganizing.  Took all the loose scripts in the top level of Scripts, and sorted them a bit.  Anything that didn't have a direct location I put under a new folder called '_System' that will show up as the first folder in the file structure.
    
    Added function to turn off Interact Light.  Basically in the InteractionAnimation script, I have a new variable that will shut off the light if it is set.  Though upon reentering a scene, it will show up as the same most likely.  Will do some more work on that in the future.
    
    Made the Scarecrows collider bigger and made it higher in the order in hopes that it is above the interactable wheat colliders.
    
    Fixed bug where if you clicked on something that was a different icon and then clicked back on the ground, it will still show the other icon.  now it will properly display the normal cursor when you click on the ground in general.

---
+++ Assets/Code/Scripts/_System/GameManager.cs
@@ -0,0 +1,251 @@
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Ink.Runtime;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : Singleton<GameManager>
+{
+    // Defining the state at game start
+    public bool wearingNightgown = true;
+
+    // Ink story
+    public TextAsset inkAsset;
+    public Story inkStory;
+
+    // Our persistent inventory
+    public List<InventoryItem> inventory = new List<InventoryItem>();
+    public List<InventoryItem> inventoryCatalog;
+    public int inventoryCursor = 0;
+
+    // Quests
+	public List<IQuest> quests = new List<IQuest>();
+	
+	public Hashtable books = new Hashtable();
+	public Hashtable pages = new Hashtable();
+
+    // Used for the ChangeScene script
+	bool setDefaultPos = false;
+    Vector2 nextPosition;
+    Vector3 nextCameraPosition;
+    Vector2 nextDirection;
+	
+	public bool loadingFromSave = false;
+	
+	public GameObject player;
+	public PlayerMovement playerMovement;
+
+    protected GameManager() { }
+
+    void Awake()
+    {
+        inventoryCatalog = InventoryItemCatalog.GetInventoryItemCatalog();
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        inkAsset = Resources.Load<TextAsset>("Dialogue/MainStory");
+        inkStory = new Story(inkAsset.text);
+
+        // Listen for a change to the "tooltip" variable in an Ink script
+        // When it changes, display a tooltip with the new value
+        inkStory.ObserveVariable ("tooltip", (string varName, object newValue) => {
+            ShowTooltipWithTimeout(newValue.ToString());
+        });
+    }
+	
+	void Start()
+	{
+		if (SceneManager.GetActiveScene().name != "TitleScreen")
+		{
+			player = GameObject.Find("Player").gameObject;
+			playerMovement = player.GetComponent<PlayerMovement>();
+		}
+	}
+
+    /**
+     * Handling Scenes
+     **/
+    public void LoadScene(string scene, Vector2 toPosition, Vector3 toCameraPosition, Vector2 toDirection)
+    {
+        nextPosition = toPosition;
+        nextCameraPosition = toCameraPosition;
+        nextDirection = toDirection;
+        //nextKnot = toKnot;
+
+        SceneManager.LoadScene(scene);
+    }
+	
+	public void LoadScene(string scene)
+	{
+		setDefaultPos = true;
+		SceneManager.LoadScene(scene);
+	}
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+		player = GameObject.Find("Player").gameObject;
+		playerMovement = player.GetComponent<PlayerMovement>();
+		
+        // Find the player in the scene
+		if (!loadingFromSave && SceneManager.GetActiveScene().name != "TitleScreen")
+		{
+			NavMeshAgent navMeshAgent = player.GetComponent<NavMeshAgent>();
+
+			// Move her to the position defined in ChangeScene
+			if (!setDefaultPos)
+			{
+				navMeshAgent.Warp(nextPosition);
+				
+				Animator animator = player.transform.GetComponent<Animator>();
+				
+				animator.SetFloat("moveX", nextDirection.x);
+				animator.SetFloat("moveY", nextDirection.y);
+				
+				Camera.main.transform.position = nextCameraPosition;
+			}
+			else
+			{
+				setDefaultPos = false;
+			}
+		}
+		else
+		{
+			//Awake();
+			playerMovement = null;
+			loadingFromSave = false;
+		}
+    }
+
+    /**
+     * Tooltips and dialogs
+     **/
+    public void ShowTooltip(string newText)
+    {
+        // Find the Tooltip object in the scene
+        GameObject uiCanvas = GameObject.Find("UICanvas").gameObject;
+        GameObject tooltip = uiCanvas.transform.Find("Tooltip").gameObject;
+
+        // In that Tooltip object, find the Text object
+        Text tooltipText = tooltip.transform.Find("TooltipText").gameObject.GetComponent<Text>();
+
+        // Update the tooltip text
+        tooltipText.text = newText;
+
+        // Show the tooltip!
+        tooltip.SetActive(true);
+    }
+
+    public void ShowTooltipWithTimeout(string newText)
+    {
+        // Find the Tooltip object in the scene
+        GameObject uiCanvas = GameObject.Find("UICanvas").gameObject;
+        GameObject tooltip = uiCanvas.transform.Find("Tooltip").gameObject;
+
+        // In that Tooltip object, find the Text object
+        Text tooltipText = tooltip.transform.Find("TooltipText").gameObject.GetComponent<Text>();
+
+        // Update the tooltip text
+        tooltipText.text = newText;
+
+        // Show the tooltip!
+        tooltip.SetActive(true);
+
+        // Start this special function to hide the tooltip after 5 secs
+        StartCoroutine(TimeoutTooltip());
+    }
+
+    public void HideTooltip()
+    {
+        // Find the Tooltip object in the scene
+        GameObject uiCanvas = GameObject.Find("UICanvas").gameObject;
+        GameObject tooltip = uiCanvas.transform.Find("Tooltip").gameObject;
+
+        // Hide the tooltip
+        tooltip.SetActive(false);
+    }
+
+    IEnumerator TimeoutTooltip()
+    {
+        // Because this function has a timer in it, it's considered a
+        // "coroutine" and needs to be an IEnumerator function to run.
+        // We can't put coroutines in normal "void" functions.
+        yield return new WaitForSeconds(5);
+        HideTooltip();
+    }
+
+    public int GetInventoryCount(string itemName)
+    {
+        return inventory.Exists(i => i.itemName == itemName) ? inventory.Find(i => i.itemName == itemName).count : 0;
+    }
+	
+	public List<InventoryItem> GetInventory()
+	{
+		return inventory;
+	}
+	
+	public void SetInventory(List<InventoryItem> loadInventory)
+	{
+		inventory = loadInventory;
+	}
+	
+	public bool HaveQuest(string checkQuest)
+	{
+		for (int q = 0; q < quests.Count; q++)
+		{
+			if (quests[q].questData.questName == checkQuest)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public List<QuestData> GetQuests()
+	{
+		List<QuestData> questData = new List<QuestData>();
+		for (int q = 0; q < quests.Count; q++)
+		{
+			questData.Add(quests[q].questData);
+		}
+		return questData;
+	}
+	
+	public void SetQuests(List<QuestData> questData)
+	{
+		for (int q = 0; q < questData.Count; q++)
+		{
+			IQuest quest = QuestType.SetQuestType(questData[q].questName);
+			quest.questData = questData[q];
+			quests.Add(quest);
+		}
+	}
+	
+	public void SetPlayerImmobilized(bool state)
+	{
+		if (playerMovement != null)
+		{
+			playerMovement.immobilized = state;
+		}
+	}
+	
+	public void SetBooks(Hashtable bookData)
+	{
+		books = bookData;
+	}
+	
+	public Hashtable GetBooks()
+	{
+		return books;
+	}
+	
+	public void SetPages(Hashtable pageData)
+	{
+		pages = pageData;
+	}
+	
+	public Hashtable GetPages()
+	{
+		return pages;
+	}
+}
---
+++ Assets/Code/Scripts/_System/Interaction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interaction : MonoBehaviour
+{
+    public Sprite interactionIcon;
+    public Sprite interactionIconActive;
+    public bool moveToTarget = false;
+    public GameObject blockedBy;
+
+    public abstract void Interact();
+}
