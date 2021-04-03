fc5d14077649c58f79fd5cc3d70bce83e2aea5e7
melanie <melanie@cooperbold.com>
2021-04-03 15:39:58 -0700

    Update Brass Book pages

--- Assets/Code/Scripts/Books/PianoPages.cs
+++ Assets/Code/Scripts/Books/PianoPages.cs
@@ -12,7 +12,7 @@ public class PianoPages : IPages
 		pages = new List<string>();
 		pages.Add("The piano is a musical instrument. The player makes sound by pressing the black and white keys.");
 		pages.Add("The keys are attached to hammers, which hit bundles of string. The strings make beautiful sounds. You can make the sounds longer by pressing a pedal with your foot.");
-		pages.Add("Many modern pianos have 88 keys. That's a lot! A person who plays piano professionally--a pianist--needs all 88 of them.");
+		pages.Add("Many modern pianos have 88 keys. That's a lot! A person who plays piano professionally - a pianist - needs all 88 of them.");
 	}
 	
 	public void SetPagesFound(int num)
