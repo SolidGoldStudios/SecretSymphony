8bd724035b060f8c727bd974c91a346221d01194
ploppowaffles <30304558+ploppowaffles@users.noreply.github.com>
2021-04-03 16:01:40 -0700

    initial fairy points of interest, livingroom and farm outdoor only so far

--- Assets/Code/Scripts/_System/FairyFollow.cs
+++ Assets/Code/Scripts/_System/FairyFollow.cs
@@ -5,17 +5,51 @@ using UnityEngine;
 public class FairyFollow : MonoBehaviour
 {
     public Transform target;
-    public float smoothTime = 0.3f;
-    private Vector3 velocity = Vector3.zero;
+    public Transform[] pointsOfInterest;
+    public float smoothTime = 1f;
+    public Vector3 velocity = Vector3.zero;
+    private List<Vector3> pointOfInterestPositions = new List<Vector3>();
 
     void Start()
     {
         transform.position = target.TransformPoint(new Vector3(-1f, 3f, 0f));
+
+        foreach (Transform pointOfInterest in pointsOfInterest)
+        {
+            StartDialogue dialogue = pointOfInterest.gameObject.GetComponent<StartDialogue>();
+            if (dialogue != null && dialogue.questGiver)
+            {
+                // Float above and to the left of NPCs with StartDialogue and questGiver
+                pointOfInterestPositions.Add(pointOfInterest.TransformPoint(new Vector3(-1f, 3f, 0f)));
+            } 
+            else
+            {
+                // Hover directly above objects
+                pointOfInterestPositions.Add(pointOfInterest.TransformPoint(new Vector3(0f, 1f, 0f)));
+            }
+        }
     }
 
     void Update()
     {
         Vector3 targetPosition = target.TransformPoint(new Vector3(-1f, 3f, 0f));
+        float minDistance = 10f;
+
+        for (int i = 0; i < pointsOfInterest.Length; i++)
+        {
+            Transform pointOfInterest = pointsOfInterest[i];
+
+            if (!pointOfInterest.gameObject.activeInHierarchy) continue;
+
+            Vector3 distance = target.position - pointOfInterest.position;
+            if (distance.magnitude < minDistance)
+            {
+                minDistance = distance.magnitude;
+                targetPosition = pointOfInterestPositions[i];
+            }
+        }
+
+        velocity = Vector3.ClampMagnitude(velocity, 10f);
 
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
