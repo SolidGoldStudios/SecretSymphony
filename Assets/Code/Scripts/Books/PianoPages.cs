c6d4a7fd51287dba9d8869177e1444788546ce05
melanie <melanie@cooperbold.com>
2021-04-03 15:31:17 -0700

    Remove empty page from Piano Book

--- Assets/Code/Scripts/Books/PianoPages.cs
+++ Assets/Code/Scripts/Books/PianoPages.cs
@@ -11,9 +11,8 @@ public class PianoPages : IPages
 	{
 		pages = new List<string>();
 		pages.Add("The piano is a musical instrument. The player makes sound by pressing the black and white keys.");
-		pages.Add("The keys are attached to hammers, which hit bundles of string. The strings make beautiful sound. You can make the sounds longer by pressing a pedal with your foot.");
+		pages.Add("The keys are attached to hammers, which hit bundles of string. The strings make beautiful sounds. You can make the sounds longer by pressing a pedal with your foot.");
 		pages.Add("Many modern pianos have 88 keys. That's a lot! A person who plays piano professionally--a pianist--needs all 88 of them.");
-		pages.Add("More Info about Pianos");
 	}
 	
 	public void SetPagesFound(int num)
