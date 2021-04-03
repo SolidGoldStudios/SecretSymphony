ba49d10ff19a78b3403461deeabdfb3562c6181f
melanie <melanie@cooperbold.com>
2021-04-03 15:17:04 -0700

    Make TitleScene go to IdleScene (and back). Tart up the Test Quest menu

---
+++ Assets/Code/Scripts/Cinematics/TitleIdle.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleIdle : MonoBehaviour
+{
+    void OnEnable()
+    {
+        GameManager.Instance.LoadScene("IdleScreen", new Vector2(-2.571f, -62.41f), new Vector3(-8f, -8.17f, -10f), new Vector2(0, 0));
+    }
+}
