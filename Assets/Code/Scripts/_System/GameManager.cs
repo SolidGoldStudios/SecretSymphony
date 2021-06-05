4cc4ce84a883c0ea197705d96bd53cf5aea115ba
melanie <melanie@cooperbold.com>
2021-06-05 13:26:42 -0700

    Add dialog prompt to wreck shit when you walk outside with the scythe for the first time

--- Assets/Code/Scripts/_System/GameManager.cs
+++ Assets/Code/Scripts/_System/GameManager.cs
@@ -10,6 +10,7 @@ public class GameManager : Singleton<GameManager>
 {
     // Defining the state at game start
     public bool wearingNightgown = true;
+	public bool hasPromptedDestruction = false;
 
     // Ink story
     public TextAsset inkAsset;
