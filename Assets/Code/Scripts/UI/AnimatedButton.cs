2d13dff023e18411d789612babcfee24ea94ff13
melanie <melanie@cooperbold.com>
2021-06-13 10:20:32 -0700

    Style trvia audio and image questions, add content

---
+++ Assets/Code/Scripts/UI/AnimatedButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimatedButton : MonoBehaviour
+{
+    private Button triggerButton;
+    public Image imageToAnimate;
+    private Animator imageAnimator;
+
+    private void Start()
+    {
+        imageAnimator = imageToAnimate.GetComponent<Animator>();
+        triggerButton = GetComponent<Button>();
+        triggerButton.onClick.AddListener(() => StartCoroutine(AnimateButtonImage()));
+    }
+
+    IEnumerator AnimateButtonImage()
+    {
+        Debug.Log("I was clicked!");
+       
+        imageAnimator.enabled = true;
+
+        yield return new WaitForSeconds(3.45f);
+
+        //imageAnimator.Play("Phono-Idle");
+        imageAnimator.enabled = false;
+    }
+
+
+
+}
