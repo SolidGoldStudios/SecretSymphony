cd7a3aa5d816d1c37cd9a0fd9b510b7b263fddb6
melanie <melanie@cooperbold.com>
2021-06-13 16:42:48 -0700

    Create scroll background, toggle between open & closed state

---
+++ Assets/Code/Scripts/UI/ToggleInstrumentScroll.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleInstrumentScroll : MonoBehaviour
+{ 
+    public Image scrollOpenBG;
+    public Image scrollClosedBG;
+    public GameObject menuButton;
+
+    private Button toggleButton;
+
+    void Start()
+    {
+        toggleButton = GetComponent<Button>();
+        toggleButton.onClick.AddListener(ToggleScroll);
+    }
+
+    void ToggleScroll()
+    {
+        if (scrollOpenBG.enabled == true)
+        {
+            scrollOpenBG.enabled = false;
+            scrollClosedBG.enabled = true;
+            menuButton.SetActive(true);
+        }
+        else
+        {
+            scrollOpenBG.enabled = true;
+            scrollClosedBG.enabled = false;
+            menuButton.SetActive(false);
+        }
+    }
+
+}
