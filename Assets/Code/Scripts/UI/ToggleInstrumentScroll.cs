7881d57b0a2225a5e853d70a957d83ef363b9563
melanie <melanie@cooperbold.com>
2021-06-13 18:00:45 -0700

    Add all instruments to scroll

--- Assets/Code/Scripts/UI/ToggleInstrumentScroll.cs
+++ Assets/Code/Scripts/UI/ToggleInstrumentScroll.cs
@@ -8,13 +8,17 @@ public class ToggleInstrumentScroll : MonoBehaviour
     public Image scrollOpenBG;
     public Image scrollClosedBG;
     public GameObject menuButton;
+    public GameObject InstrumentContainer;
 
     private Button toggleButton;
+    private RectTransform rect;
 
     void Start()
     {
         toggleButton = GetComponent<Button>();
         toggleButton.onClick.AddListener(ToggleScroll);
+
+        rect = GetComponent<RectTransform>();
     }
 
     void ToggleScroll()
@@ -24,12 +28,16 @@ public class ToggleInstrumentScroll : MonoBehaviour
             scrollOpenBG.enabled = false;
             scrollClosedBG.enabled = true;
             menuButton.SetActive(true);
+            InstrumentContainer.SetActive(false);
+            rect.anchoredPosition3D = new Vector3(-49f, 0f, 0f);
         }
         else
         {
             scrollOpenBG.enabled = true;
             scrollClosedBG.enabled = false;
             menuButton.SetActive(false);
+            InstrumentContainer.SetActive(true);
+            rect.anchoredPosition3D = new Vector3(-306f, 0f, 0f);
         }
     }
 
