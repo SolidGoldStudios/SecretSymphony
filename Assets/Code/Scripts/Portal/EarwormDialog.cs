37f8dd654e0672c2b8ca12e8cfbdd7e934b3d6cb
melanie <melanie@cooperbold.com>
2021-08-28 14:08:37 -0700

    Oh my god I never merged my portal stuff

---
+++ Assets/Code/Scripts/Portal/EarwormDialog.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarwormDialog : MonoBehaviour
+{   
+    public GameObject dialogBox;
+    public GameObject timelineObject;
+
+    void Start()
+    {
+        dialogBox.SetActive(true);
+        NPCDialogue npcDialogue = dialogBox.GetComponent<NPCDialogue>();
+
+        // Check to see if we have already spoken to the Earworm
+        if (!GameManager.Instance.hasMetEarworm)
+        { 
+            npcDialogue.ShowDialog("finalexam_earworm");
+            GameManager.Instance.hasMetEarworm = true;
+            timelineObject.SetActive(true);
+        } else {
+            timelineObject.SetActive(false);
+            npcDialogue.ShowDialog("finalexam_fairy");
+        }
+    }
+}
